f88b64c63e79d37bf201795626322fceddeb6bc7
Vishuarora17 <va66753@gmail.com>
2024-11-17 15:50:33 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Hello {
+	public static void main(String arg[]) {
+		System.out.println("*****************************");
+		System.out.println("\tRadhe Radhe");
+		System.out.println("*****************************");
+	}
+
+}
